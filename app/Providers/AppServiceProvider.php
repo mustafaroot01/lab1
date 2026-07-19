ea7601ca99625e267450dfc81feceaa0ee00e689
MRootxx <dr.rootvb@gmail.com>
2026-07-20 01:28:17 +0300

    fix: dynamically load uploaded firebase credentials file

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,5 +22,14 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Model::preventLazyLoading(! $this->app->isProduction());
+
+        // استخدام Pagination من Bootstrap
+        Paginator::useBootstrapFive();
+        
+        // إعداد مسار ملف الاعتماد لـ Firebase الديناميكي الذي تم رفعه من الداشبورد
+        $firebaseCredentialsPath = storage_path('app/private/firebase/firebase-credentials.json');
+        if (file_exists($firebaseCredentialsPath)) {
+            config(['firebase.projects.app.credentials' => $firebaseCredentialsPath]);
+        }
     }
 }
